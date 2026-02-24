ee6124a1931d5e3a87e64ac7ce9a2ea2cb50710f
Khaeshi <kagtabss@gmail.com>
2026-02-24 17:46:29 +0800

    Deployed Frontend on Vercel and Backend on Render added Analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Playfair_Display, Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${playfair.variable} ${inter.variable} antialiased`}>
+      <Analytics/>
         <AuthProvider>
             <CartProvider>
               {children}
