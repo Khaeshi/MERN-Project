9eb21ba3310adf094c45a85a8ce612f3aae5641c
Khaeshi <kagtabss@gmail.com>
2026-02-24 20:09:59 +0800

    set credentials from strict to none this issue is because the back end is on Render and the frontend is on vercel hence JWT token is restricted because origin is set to strict

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,7 +14,7 @@ const setTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
     path: '/',
   };
